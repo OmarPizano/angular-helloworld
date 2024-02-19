80bacf233343b2bea4da780d3ff6265969e96d04
OmarPizano <oj.pizanoorozco@ugto.mx>
2024-02-19 11:10:36 -0600

    extraer config de la db de las variables de entorno

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:toor@127.0.0.1/helloworld'
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DB_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
