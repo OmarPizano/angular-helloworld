b620a6295c4a11f238d04ca4573a0cc338c5592f
OmarPizano <oj.pizanoorozco@ugto.mx>
2024-02-25 15:58:29 -0600

    Refactorizar backend API usando flask-restful, fix #1
    La librería atrapa la mayoría de errores dependiendo del estado devuelto o del tipo de solicitud

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, abort, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource, reqparse
 from dotenv import load_dotenv
 from os import getenv
 
@@ -9,50 +10,70 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DB_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
 CORS(app)
 
+db = SQLAlchemy(app)
+api = Api(app)
+
 # modelo
-class Names(db.Model):
+class Name(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
 
-    def __init__(self, name: str) -> None:
-        self.name = name
+    def __init__(self, name:str) -> None:
+       self.name = name 
 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
 
 with app.app_context():
     db.create_all()
 
-# rutas
-@app.route('/names', methods=['GET'])
-def get_names():
-    names = Names.query.all()
-    results = [{'id': name.id, 'name': name.name} for name in names]
-    return jsonify(results)
+# recurso
+class Names(Resource):
+    def __init__(self) -> None:
+        self.parser = reqparse.RequestParser() 
 
-@app.route('/names', methods=['POST'])
-def create_name():
-    data = request.json
-    new_name = Names(name=data['name'])
-    db.session.add(new_name)
-    db.session.commit()
-    return jsonify({"id": new_name.id, "name": new_name.name})
+    def get(self):
+        names = Name.query.all()
+        results = [name.to_dict() for name in names]
+        return results, 200
 
-@app.route('/names/<int:id>', methods=['DELETE'])
-def delete_name(id):
-    name = Names.query.get(id)
-    db.session.delete(name)
-    db.session.commit()
-    return jsonify({"id": name.id, "name": name.name})
+    def post(self):
+        self.parser.add_argument('name', type=str, required=True)
+        args = self.parser.parse_args()
+        new_name = Name(name=args['name'])
+        db.session.add(new_name)
+        db.session.commit()
+        return new_name.to_dict(), 201
 
-@app.route('/names/<int:id>', methods=['PUT'])
-def update_name(id):
-    name = Names.query.get(id)
-    name.name = request.json['newName']
-    db.session.commit()
-    return jsonify({"id": name.id, "name": name.name})
+    def delete(self, id: int = 0):
+        if id == 0:
+            abort(400)
+        name_to_delete = Name.query.get(id)
+        if not name_to_delete:
+            abort(404)
+        db.session.delete(name_to_delete)
+        db.session.commit()
+        return {}, 204
+
+    def put(self, id: int = 0):
+        if id == 0:
+            abort(400)
+        self.parser.add_argument('newName', type=str, required=True)
+        args = self.parser.parse_args()
+        name_to_update = Name.query.get(id)
+        if not name_to_update:
+            abort(404)
+        name_to_update.name = args['newName']
+        db.session.commit()
+        return {}, 204
+
+# rutas permitidas
+api.add_resource(Names, '/names', '/names/<int:id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
