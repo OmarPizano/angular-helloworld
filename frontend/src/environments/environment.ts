f557dcdbf54bca698aaa9461a233b61d6bca80cc
OmarPizano <oj.pizanoorozco@ugto.mx>
2024-03-05 18:03:29 -0600

    Agregar url del backend al entorno de producción

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // TODO: ajustar cuando se tenga la URL de prod
-  apiURL: 'http://127.0.0.1:5000'
+  apiURL: 'https://angular-helloworld-backend.vercel.app/'
 };
