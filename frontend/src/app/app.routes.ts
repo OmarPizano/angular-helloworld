e2d1872c684c3fcc806e934031c604b94312ddb1
OmarPizano <oj.pizanoorozco@ugto.mx>
2024-03-21 05:13:44 -0600

    Agregar guarda de ruta para el recurso de nombres

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,10 +4,11 @@ import { AboutComponent } from './components//about/about.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'name-list', component: NameListComponent },
+  { path: 'name-list', component: NameListComponent, canActivate: [AuthGuard] },
   { path: 'about', component: AboutComponent },
   { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '' },
