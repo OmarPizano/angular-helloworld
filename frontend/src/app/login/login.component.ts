96a79de456be54e9270ced19d1c91320dfcba6d8
OmarPizano <oj.pizanoorozco@ugto.mx>
2024-03-26 15:26:59 -0600

    refactor(login-form): cambiar a formularios reactivos

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,22 +1,27 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
   title: string = 'Login';
-  username: string = '';
-  password: string = '';
 
-  constructor(private authService: AuthService, private router: Router) { }
+  loginForm = this.fb.group({
+    username: [''],
+    password: ['']
+  })
 
-  login(username: string, password: string): void {
+  constructor(private authService: AuthService, private router: Router, private fb: FormBuilder) { }
+
+  login(): void {
+    const username = this.loginForm.value['username'] || '';
+    const password = this.loginForm.value['password'] || '';
     this.authService.generateUserToken(username, password).subscribe({
       next: (data) => {
         this.authService.saveToken(data.token);
