a88173c7d7827c94b6aa5f93cb2d678bf9a37839
OmarPizano <oj.pizanoorozco@ugto.mx>
2024-04-04 13:29:08 -0600

    feat(home): mostrar API version y lista de nombres en componente HOME
    Usar temporalmente el cliente http directo.

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NamesService } from '../../services/names.service';
+import { Name } from '../../models/name';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +10,27 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   title: string = 'Home Page';
+  nameList: Name[] = [];
+  apiVersion: string = '';
+
+  // temporalmente incluimos el cliente http ya que ningun servicio consulta
+  // la ruta para obtener la versión
+  constructor(private nameService: NamesService, private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.nameService.getNames().subscribe(
+      (names) => {
+        this.nameList = names;
+      }
+    );
+    // TODO: poner esta petición en un servicio
+    this.httpClient.get<{api_version: string}>(environment.apiURL).subscribe(
+      (version) => {
+        this.apiVersion = version.api_version;
+      }
+    )
+  }
+
 }
