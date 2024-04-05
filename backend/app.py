5f271abcb70089a70bec4b09ec0d023c53fa6b1b
OmarPizano <oj.pizanoorozco@ugto.mx>
2024-04-05 16:49:01 -0600

    feat(backend-cors): agregar configuración de orígenes
    Esta es una configuración de prueba para el entorno de producción.

--- backend/app.py
+++ backend/app.py
@@ -19,7 +19,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = getenv('JWT_SECRET')
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)
 
-CORS(app)
+CORS(app, resources={
+    r"/api$": {"origins": "*"},
+    r"/api/[a-z]+": {"origins": "http://angular-helloworld.duckdns.org"},
+    })
 jwt = JWTManager(app)
 bcrypt = Bcrypt(app)
 
